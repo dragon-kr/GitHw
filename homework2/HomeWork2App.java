48f54e1311a83a309cca11ac3415b11c6ddecec6
Mikhail <dragon-kr@mail.ru>
2022-05-10 23:28:16 +0300

    Домашка №2

---
+++ homework2/HomeWork2App.java
@@ -0,0 +1,55 @@
+package homework2;
+
+public class HomeWork2App {
+//    public static void main(String[] args) {
+//        taskOne();
+//    }
+//
+//    public static boolean taskOne(int a, int b) {
+//        return a + b >= 10 && a + b <= 20;
+//    }
+//
+//    public static void taskOne() {
+//        int a = 5;
+//        int b = 10;
+//        int sum = a + b;
+//        if (sum >= 10 && sum <= 20) {
+//            System.out.println(true);
+//        } else {
+//            System.out.println(false);
+//        }
+//        System.out.println("********");
+//
+//        taskTwo();
+//    }
+//
+//    public static void taskTwo() {
+//        int a = 5;
+//        if (a >= 0) {
+//            System.out.println("Число " + a + " - положительное");
+//        } else {
+//            System.out.println("Число " + a + " - отрицательное");
+//        }
+//
+//
+//        System.out.println("********");
+//
+//    }
+
+    public static void main(String[] args) {
+//        System.out.println(taskThree(-5));
+//    }
+//    static boolean taskThree(int a) {
+//        if (a < 0) {
+//            return true;
+//            } else {
+//            return false;
+//        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Привет");
+
+        }
+    }
+}
+
+
